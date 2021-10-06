7d8e6566207a193a9e7f0f86d9abcfba3fc704c7
SuperHitius <tarakanovmixail@mail.ru>
2021-10-06 22:38:05 +0300

    FIX: Мобильная верстка футора с мелкими правками

--- js/main.js
+++ js/main.js
@@ -33,7 +33,6 @@ new Swiper('.reviews-slider', {
 
 var menuButton = document.querySelector(".menu-button");
 menuButton.addEventListener('click', function () {
-    console.log('Клик по кнопке меню');
     document
     .querySelector(".navbar-bottom")
     .classList.toggle("navbar-bottom--visible");
